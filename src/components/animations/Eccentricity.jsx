e0a70d5b29361948f670df69c9dbf15a227cf447
AkipTsaqif <akiptsaqif@gmail.com>
2023-04-13 21:50:47 +0700

    eksentrisitas v2.5

--- src/components/animations/Eccentricity.jsx
+++ src/components/animations/Eccentricity.jsx
@@ -1,19 +1,19 @@
-import { useEffect, useState } from "react";
-import { Slider, Typography } from "@mui/material";
-import useDidMountEffect from "../../hooks/useDidMountEffect";
+import { useEffect, useRef, useState } from 'react';
+import { Slider, Typography } from '@mui/material';
+import useDidMountEffect from '../../hooks/useDidMountEffect';
 
 const marks = [
 	{
 		value: 0,
-		label: "Lingkaran",
+		label: 'Lingkaran',
 	},
 	{
 		value: 1,
-		label: "Parabola",
+		label: 'Parabola',
 	},
 	{
 		value: 2,
-		label: "Hiperbola",
+		label: 'Hiperbola',
 	},
 ];
 
@@ -27,65 +27,55 @@ const Eccentricity = () => {
 	const now = Date.now() / 1000;
 
 	useEffect(() => {
-		// if (ecc >= 0.01) {
 		setRx(10 + 85 * ecc);
 		setRy(10 + 50 * ecc);
 		setCx(50 - 85 * ecc);
-		// }
 	}, [ecc]);
 
 	useEffect(() => {
-		const orbitRadiusX = rx + 1; // radius of the ellipse + radius of the circle
-		const orbitRadiusY = ry + 1; // radius of the ellipse + radius of the circle
+		const orbitRadiusX = 10 + 85 * ecc; // radius of the ellipse + radius of the circle
+		const orbitRadiusY = 10 + 50 * ecc; // radius of the ellipse + radius of the circle
+		const ncx = 50 - 85 * ecc;
 
 		// Calculate new coordinates for the orbiting object
-		const newCx = 50 + orbitRadiusX * Math.cos(angle);
+		const newCx = ncx + orbitRadiusX * Math.cos(angle);
 		const newCy = 50 + orbitRadiusY * Math.sin(angle);
-		setAngle(angle + 0.01); // Increment the angle for the next frame
+		setAngle(angle - 0.0005); // Increment the angle for the next frame
 
 		// Update the cx and cy attributes of the orbiting object
-		const orbitingObject = document.getElementById("orbiting-object");
-		orbitingObject.setAttribute("cx", newCx);
-		orbitingObject.setAttribute("cy", newCy);
-	}, [angle, rx, ry]);
+		const orbitingObject = document.getElementById('orbiting-object');
+		orbitingObject.setAttribute('cx', newCx);
+		orbitingObject.setAttribute('cy', newCy);
+	}, [angle, ecc]);
 
 	useEffect(() => console.log({ ecc, cx, rx, ry }), [cx, rx, ry]);
 
 	return (
-		<div>
-			<div style={{ width: "600px", height: "600px" }}>
-				<svg viewBox="0 0 100 100">
+		<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+			<div style={{ width: '600px', height: '600px' }}>
+				<svg viewBox='0 0 100 100'>
 					<circle
-						cx="50"
-						cy="50"
-						r="3"
-						stroke="grey"
-						strokeWidth="0.01"
-						fill="yellow"
+						cx='50'
+						cy='50'
+						r='3'
+						stroke='grey'
+						strokeWidth='0.01'
+						fill='yellow'
 					/>
 					<ellipse
-						id="inner-planet"
+						id='inner-planet'
 						cx={cx}
-						cy="50"
+						cy='50'
 						rx={rx}
 						ry={ry}
-						stroke="black"
-						strokeWidth="0.1"
-						fill="none"
+						stroke='black'
+						strokeWidth='0.1'
+						fill='none'
 					/>
-					<ellipse
-						cx="48"
-						cy="50"
-						rx="25"
-						ry="23"
-						stroke="black"
-						strokeWidth="0.1"
-						fill="none"
-					/>
-					<circle id="orbiting-object" r="1" fill="red" />
+					<circle id='orbiting-object' r='1' fill='red' />
 				</svg>
 			</div>
-			<div style={{ width: "50vw", margin: "auto" }}>
+			<div style={{ width: '600px', margin: 'auto', marginLeft: 'auto' }}>
 				<Typography>Eksentrisitas:</Typography>
 				<Slider
 					max={2}
@@ -93,7 +83,7 @@ const Eccentricity = () => {
 					marks={marks}
 					step={0.01}
 					value={ecc}
-					valueLabelDisplay="auto"
+					valueLabelDisplay='auto'
 					onChange={(e, val) => {
 						setEcc(val);
 					}}
